359c021ae16cb153b8abcd881ad7814394e22901
dgroz <dgrozier24@gmail.com>
2021-02-11 07:07:13 -0700

    second commit -- Practicum 2

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -6,5 +6,19 @@ output: 3
 */
 
 var median = function(input) {
-    
+    input.sort()
+    if (input.length === 1)   {
+        return input[0]
+    }
+    else if ( input.length === 0)   {
+        return 0
+    }
+    else if (input.length % 2 === 0) {
+        firstIndex = (input.length/2) -1
+        secondIndex = input.length/2
+        return (input[firstIndex] + input[secondIndex])/2
+    } else {
+        middleIndex = (input.length/2) -.5
+        return input[middleIndex]
+    }
 };
--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -11,5 +11,43 @@ output: 4
 */
 
 var maxSumOfMins = function(input) {
-
+       curArr = input
+       totalSum = 0
+       while (curArr.length > 0)  {
+              firstMaxNum = max(curArr)
+              curArr.splice(curArr.indexOf(firstMaxNum), 1)
+              secondMaxNum = max(curArr)
+              curArr.splice(curArr.indexOf(secondMaxNum), 1)
+              curPair = [firstMaxNum, secondMaxNum]
+              totalSum += min(curPair)
+       }
+       return totalSum
 };
+
+
+var min = function(arr)       {
+       curLowest = null
+       for (i=0; i<arr.length; i++)    {
+           if (curLowest === null) {
+               curLowest = arr[i]
+           }
+           else if (arr[i] < curLowest) {
+               curLowest = arr[i]
+           }
+       }
+       return curLowest
+}
+   
+   
+var max = function(arr)       {
+       curHighest = null
+       for (i=0; i<arr.length; i++)    {
+           if (curHighest === null) {
+               curHighest = arr[i]
+           } else if (arr[i] > curHighest) {
+               curHighest = arr[i]
+           }
+       }
+       return curHighest
+}
+
